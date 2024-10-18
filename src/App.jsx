aa7643c65d4b342cf25b3f08e29f531e93b4aca5
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2024-10-18 01:46:56 +0100

    Scaffolded project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/base/base.css";
+
+function App() {
+	return (
+		<div className="wrapper">
+			<div className="dashb">dashb</div>
+            <div className="main">main</div>
+		</div>
+	);
+}
+
+export default App;
