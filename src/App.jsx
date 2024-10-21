7184864af019efd03a324f92d534e6419fc5f977
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2024-10-21 03:36:25 +0100

    added search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./styles/base/base.css";
 import Dashboard from './components/Dashboard/Dashboard';
+import Main from './components/Main/Main';
 
 function App() {
 	return (
 		<div className="wrapper">
 			<Dashboard />
-            <div className="main">main</div>
+            <Main />
 		</div>
 	);
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './styles/main.css';
+
+function Main() {
+	return (
+		<div className="main">
+			<div className="searchBar">
+				<input type="text" placeholder="Search..." />
+				<ion-icon name="search-outline"></ion-icon>
+			</div>
+		</div>
+	);
+}
+
+export default Main;
